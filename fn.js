9831cc86cf119b6e1cccf16b2fa4782e34d54c15
Dima <Dimahodanitsky@gmail.com>
2020-02-20 20:37:55 +1200

    Добавление функций в код

---
+++ fn.js
@@ -0,0 +1,29 @@
+/* function showMessage(text) // аргумент который передают в функцию)
+{  
+    alert(text);//то что функция делает
+    let num = 20;
+
+  } 
+
+  showMessage("helo"); // вызов функции - сюда будет вписано то, что выводиться в аргументе */
+
+
+
+ /*  function calc(a,b) {
+    return (a+b);
+  }
+  console.log(calc(3,4));
+  console.log(calc(23,655));
+
+  function retVar() {
+    let num = 50;
+    return num;
+  }
+
+  let anotherNum = retVar();
+  console.log (anotherNum); */
+
+
+  let calc = (a,b) => a+b;
+  console.log(calc(3,557));
+  console.log(calc(23,655));
